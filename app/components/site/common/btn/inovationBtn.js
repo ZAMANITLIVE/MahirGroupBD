8713c04176602fdebbac3bc65e900f22b0d72e2c
ZAMAN IT <zamanit387@gmail.com>
2025-05-11 12:44:02 +0600

    Update inovationBtn.js

--- app/components/site/common/btn/inovationBtn.js
+++ app/components/site/common/btn/inovationBtn.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const InnovativeBtn = ({radious = " ", color = "bg-[#32BA7D]", text = "text-white"}) => {
+const InnovativeBtn = ({radious = " ", color = "bg-[#F9A61A]", text = "text-white"}) => {
     return (
         <div className='flex justify-end ml-auto space-x-2'>
             <button className={`px-5 py-2 md:px-3 md:text-sm font-inter font-medium hover:text-textMain hover:${color} border-2 ${radious} text-textBody hover:shadow-[0_2px_15px_-1px_#000033] border-primary hover:border-none`}>Show All</button>
