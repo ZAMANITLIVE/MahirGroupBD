92bc7b9968360bf71d99d56f9a20f679cb8fa673
ZAMAN IT <zamanit387@gmail.com>
2025-05-12 10:33:55 +0600

    Update partner.js

--- app/components/home1/partner.js
+++ app/components/home1/partner.js
@@ -12,16 +12,16 @@ const Partner = ({theme, bgColor='bg-[#ECFDF4]', bgOpacity= 'bg-opacity-100'}) =
   const partnerImages = setting?.partner ? [...setting.partner, ...setting.partner] : [];
   return (
     <div className={` ${bgColor}  ${theme === 'theme4' ? ' ' : 'shadow-custom-light'}  py-4 md:py-6 xl:py-8`}>
-      <h1 className='font-medium sm:text-xl md:text-2xl font-lexend  text-[#333] text-center mb-10'>{i18n.t('Trusted by over 32K growing companies')}</h1>
+      <h1 className='font-medium sm:text-xl md:text-2xl font-lexend  text-[#333] text-center mb-10'>{i18n.t('Sister Concerns of Mahir Group')}</h1>
       <Marquee speed={100} pauseOnHover={true}>
         <div className="flex items-center gap-0">
           {partnerImages.map((image, index) => (
             <div key={index} className="flex-shrink-0 mx-3 md:mx-6">
               <Image
-                className="w-[168px] h-[48px] object-contain"
+                className="w-[150px] h-[150px] object-contain"
                 src={image}
-                width={168}
-                height={48}
+                width={150}
+                height={150}
                 alt={`partner-image-${index}`}
               />
             </div>
