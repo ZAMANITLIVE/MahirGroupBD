358817a367d926114b6360915c93cbbc8c60ecf6
ZAMAN IT <zamanit387@gmail.com>
2025-05-13 17:13:05 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import Providers from "./provider/userProvider";
 import "./styles/globals.scss";
 
 export const metadata = {
-  title: "Mahir Group BD",
+  title: "Mahir Group - The largest accessories & trims manufacturer in Bangladesh",
   description:
     "Delivering excellence in garment accessories with quality and innovation.",
 };
